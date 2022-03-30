b70264fe1802282ecc9aeb8fed6a3c0516f7972b
Kaique Ramos <ramoskaique1324@gmail.com>
2022-03-29 22:20:36 -0300

    setting port dinamically with Heroku

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ io.on("connection", socket => {
 	console.log("Someone connected...");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
 	console.log("Listening on port " + PORT + "...");
 });

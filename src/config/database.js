b9e7bc725ed5451f4da67d1a391d377da49191e8
Kaique Ramos <ramoskaique1324@gmail.com>
2022-04-07 14:53:44 -0300

    Try to change the way Sequelize connects to database

--- src/config/database.js
+++ src/config/database.js
@@ -10,14 +10,11 @@ module.exports = {
 		dialect: "postgres",
 	},
 	production: {
-		username: process.env.PROD_DB_USERNAME,
-		password: process.env.PROD_DB_PASSWORD,
-		database: process.env.PROD_DB_NAME,
-		host: process.env.PROD_DB_HOST,
-		port: process.env.PROD_DB_PORT,
+		use_env_variable: process.env.DATABASE_URL,
 		dialectOptions: {
 			ssl: true,
 		},
 		dialect: "postgres",
 	},
 };
+

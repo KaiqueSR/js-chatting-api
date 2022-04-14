037dd579155f9fbf7b7810fa6ad1640a36f6d12d
Kaique Ramos <ramoskaique1324@gmail.com>
2022-04-14 19:05:58 -0300

    socket server apart

--- src/server.js
+++ src/server.js
@@ -1,15 +1,10 @@
 /* eslint-disable no-unused-vars */
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const { Message } = require("./db/models");
+const { io } = require("socket.io-client");
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-const { Message } = require("./db/models");
-
-let connectedUsers = [];
+const socket = io(process.env.SOCKET_SERVER_HOST);
 
 app.use(express.json());
 
@@ -25,40 +20,11 @@ app.post("/messages", async (req, res) => {
   // to implement yet
 });
 
-io.on("connection", socket => {
-  socket.on("user logged in", async username => {
-    connectedUsers.push({ id: socket.id, username });
-    io.emit("new user connected", username);
-    io.emit(
-      "connected users",
-      connectedUsers.map(user => user.username)
-    );
-
-    const previousMessages = await Message.findAll();
-
-    for (let message of previousMessages) {
-      socket.emit("new message", message);
-    }
-  });
-
-  socket.on("disconnect", () => {
-    connectedUsers = connectedUsers.filter(user => user.id != socket.id);
-    io.emit(
-      "connected users",
-      connectedUsers.map(user => user.username)
-    );
-  });
-
-  socket.on("send message", message => {
-    message.createdAt = new Date().getTime();
-
-    Message.create(message);
-
-    socket.broadcast.emit("new message", message);
-  });
+app.get("/users", async (req, res) => {
+  // to implement yet
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });

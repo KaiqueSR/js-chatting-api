20b2feea79714c42a0e3f1e744e3af24f422b4fb
Kaique Ramos <ramoskaique1324@gmail.com>
2022-04-14 18:34:15 -0300

    removing ejs

--- src/config/database.js
+++ src/config/database.js
@@ -1,27 +1,27 @@
 require("dotenv/config");
 
 module.exports = {
-	development: {
-		username: process.env.DEV_DB_USERNAME,
-		password: process.env.DEV_DB_PASSWORD,
-		database: process.env.DEV_DB_NAME,
-		host: "localhost",
-		port: process.env.DEV_DB_PORT,
-		dialect: "postgres",
-	},
-	production: {
-		username: process.env.PROD_DB_USERNAME,
-		password: process.env.PROD_DB_PASSWORD,
-		database: process.env.PROD_DB_NAME,
-		host: process.env.PROD_DB_HOST,
-		port: process.env.PROD_DB_PORT,
-		dialectOptions: {
-			ssl: {
-				require: true,
-				rejectUnauthorized: false,
-			},
-		},
-		dialect: "postgres",
-	},
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_NAME,
+    host: "localhost",
+    port: process.env.DEV_DB_PORT,
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    dialect: "postgres",
+  },
 };
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -10,59 +11,54 @@ const { Message } = require("./db/models");
 
 let connectedUsers = [];
 
-app.use("/static", express.static(__dirname + "/views"));
-app.use(express.urlencoded({ extended: true }));
-
-app.set("views", __dirname + "/views");
-app.set("view engine", "ejs");
+app.use(express.json());
 
 app.get("/", async (req, res) => {
-	res.render("index");
+  // to implement yet
 });
 
-app.post("/chat-room", async (req, res) => {
-	const { username } = req.body;
-	if (username) {
-		res.render("chat-room", { username });
-	} else {
-		res.redirect("/");
-	}
+app.get("/messages", async (req, res) => {
+  // to implement yet
+});
+
+app.post("/messages", async (req, res) => {
+  // to implement yet
 });
 
 io.on("connection", socket => {
-	socket.on("user logged in", async username => {
-		connectedUsers.push({ id: socket.id, username });
-		io.emit("new user connected", username);
-		io.emit(
-			"connected users",
-			connectedUsers.map(user => user.username)
-		);
+  socket.on("user logged in", async username => {
+    connectedUsers.push({ id: socket.id, username });
+    io.emit("new user connected", username);
+    io.emit(
+      "connected users",
+      connectedUsers.map(user => user.username)
+    );
 
-		const previousMessages = await Message.findAll();
+    const previousMessages = await Message.findAll();
 
-		for (let message of previousMessages) {
-			socket.emit("new message", message);
-		}
-	});
+    for (let message of previousMessages) {
+      socket.emit("new message", message);
+    }
+  });
 
-	socket.on("disconnect", () => {
-		connectedUsers = connectedUsers.filter(user => user.id != socket.id);
-		io.emit(
-			"connected users",
-			connectedUsers.map(user => user.username)
-		);
-	});
+  socket.on("disconnect", () => {
+    connectedUsers = connectedUsers.filter(user => user.id != socket.id);
+    io.emit(
+      "connected users",
+      connectedUsers.map(user => user.username)
+    );
+  });
 
-	socket.on("send message", message => {
-		message.createdAt = new Date().getTime();
+  socket.on("send message", message => {
+    message.createdAt = new Date().getTime();
 
-		Message.create(message);
+    Message.create(message);
 
-		socket.broadcast.emit("new message", message);
-	});
+    socket.broadcast.emit("new message", message);
+  });
 });
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}...`);
+  console.log(`Listening on port ${PORT}...`);
 });

ff513a4828f6b03386c901e36e082222f74fcd94
Kaique Ramos <ramoskaique1324@gmail.com>
2022-04-14 23:10:38 -0300

    GET:/messages route implemented and added custom 404 response

--- src/server.js
+++ src/server.js
@@ -1,27 +1,35 @@
 /* eslint-disable no-unused-vars */
 const express = require("express");
-const { Message } = require("./db/models");
 const { io } = require("socket.io-client");
+const { Message } = require("./db/models");
 
 const app = express();
 const socket = io(process.env.SOCKET_SERVER_HOST);
-
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  // to implement yet
+app.get("/messages", async (req, res) => {
+  const allMessages = await Message.findAll();
+
+  res.json(allMessages);
 });
 
-app.get("/messages", async (req, res) => {
-  // to implement yet
+app.get("/users", async (req, res) => {
+  
 });
+
+/*
+  AUTHENTICATION HERE
+*/
 
 app.post("/messages", async (req, res) => {
   // to implement yet
 });
 
-app.get("/users", async (req, res) => {
-  // to implement yet
+app.use((req, res) => {
+  res.status(404).json({
+    error: true,
+    statusCode: 404,
+    message: "I don't recognize this endpoint... Accepted routes: /messages (GET and POST), /users (GET)" });
 });
 
 const PORT = process.env.PORT || 5000;
